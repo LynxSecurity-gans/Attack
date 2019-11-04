1ea1826a1cdf68deb83b4db88fa29b8fc3335f53
LynxSecurity <57316494+LynxSecurity-gans@users.noreply.github.com>
2019-11-04 20:21:21 +0700

    Attack
    Perintah : python ddos.py
    Thanks for watching

---
+++ ddos.py
@@ -0,0 +1,17 @@
+import time
+import sys
+import os
+
+os.system('clear')
+print ('-----------------------------------------------')
+print ('Author : Mr.Lynx')
+print ('Github : https://github.com/LynxSecurity-gans')
+print ('Contact : lnxscrt@gmail.com')
+print ('-----------------------------------------------')
+print ('')
+ip = input('Masukan IP target : ')
+port = input('Masukan port : ')
+print ('Loading...')
+print ('-= GO =-')
+for i in range(1000000): #10 adi
+   print ('Sukses mengirim ', str(i))
